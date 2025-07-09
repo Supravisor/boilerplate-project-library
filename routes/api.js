d0a8b6d652992952fda0f6cb27f50a0342457ede
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-07-09 03:33:37 +0000

    GET

--- routes/api.js
+++ routes/api.js
@@ -8,22 +8,33 @@
 
 'use strict';
 
+let books = [];
+let counter = -1;
+
 module.exports = function (app) {
 
   app.route('/api/books')
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      let id = req.body._id;
+      for (let i = 0; i < books.length; i++) {
+        if (Number(id) === books[i]._id) {
+          return res.json([{"_id": books[i]._id, "title": books[i].book_title, "commentcount": books[i].commentcount }]);
+        }
+      }
     })
     
     .post(function (req, res){
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
-      books.push ({
+      let newBook = {
         'commentcount': 0,
         'title': title,
-        '_id': counter 
-      });
+        '_id': counter++
+      };
+      return res.json(newBook);
+
     })
     
     .delete(function(req, res){
